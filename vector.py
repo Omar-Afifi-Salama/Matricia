eeb16dedfc24a3feda6b1633f38456d10753f0ee
Omar Afifi <omarafifi2030@gmail.com>
2026-04-11 00:23:54 +0200

    Clean up the code in the vector class. Add a way to check for the parallelity of vectors.

--- vector.py
+++ vector.py
@@ -10,7 +10,7 @@ class Vector:
     _name_map = {"x": 0, "y": 1, "z": 2, "w": 3,
                  "a": 0, "b": 1, "c": 2, "d": 3}
     
-    def __init__(self, *args, mutable=False, convert_to_float=False, **kwargs):
+    def __init__(self, *args, mutable=False, **kwargs):
 
         self.mutable = mutable
         temp_list = [] # initialized empty early in case of no args or kwargs to handle Vector()
@@ -20,13 +20,13 @@ class Vector:
                 arg = args[0]
                 # handles Vector([1, 2, 3]) or Vector(generator)
                 if isinstance(arg, (list, tuple)) or hasattr(arg, "__iter__"):
-                    temp_list = [float(x) for x in arg] if convert_to_float else list(arg)
+                    temp_list = [float(x) for x in arg]
                 else:
                     # handles Vector(5)
-                    temp_list = [float(arg)] if convert_to_float else [arg]
+                    temp_list = [float(arg)]
             else:
                 # handles Vector(1, 2, 3)
-                temp_list = [float(x) for x in args] if convert_to_float else list(args)
+                temp_list = [float(x) for x in args]
 
         if kwargs: # handles Vector(x=1, y=2, z=3)
             for key, value in kwargs.items():
@@ -34,11 +34,11 @@ class Vector:
                     idx = Vector._name_map[key]
                     while len(temp_list) <= idx: # handles Vector(z=2) by creating temp_list = [0,0,2]
                         temp_list.append(0)
-                    temp_list[idx] = float(value) if convert_to_float else value
+                    temp_list[idx] = float(value)
 
         self.components = temp_list if self.mutable else tuple(temp_list)
-        
     
+    # TODO merge them
     def _validate_vector(self, other):
         if not isinstance(other, Vector):
             return False
@@ -47,8 +47,24 @@ class Vector:
             raise ValueError(
                 f"Cannot perform operation on vectors of of different lengths: {len(self.components)} and {len(other.components)}"
             )
+        
         return True
     
+    @staticmethod
+    def same_dimensions(*vectors):
+
+        if len(vectors) < 2:
+            return True # a vector to itself is of the same dimensions
+        
+        for i, v in enumerate(vectors):
+            if not isinstance(v, Vector):
+                raise TypeError(f"Argument at index {i} is {type(v).__name__}, not a Vector.")
+        
+        if not all(len(vectors[0]) == len(v) for v in vectors[1:]):
+            return False
+        
+        return True
+
     def _get_fractions(self):
         return [(f.numerator) if (f := Fraction(x).limit_denominator(Vector.DISPLAY_PRECISION)).denominator == 1 else (f) for x in self.components]
 
@@ -64,14 +80,18 @@ class Vector:
     def __setitem__(self, index, value):
         if not self.mutable:
             raise TypeError("This vector is immutable.")
-        self.components[index] = value
+        
+        # expands the list if the index is outside the list so [1, 2, 3] and v[6] = 4 then [1, 2, 3, 0, 0, 0, 4]
+        while len(self.components) <= index:
+            self.components.append(0.0)
+        
+        self.components[index] = float(value)
 
     def __setattr__(self, name, value):
         if not self.mutable:
             raise AttributeError("This vector is immutable.")
         
         if name in Vector._name_map:
-            
             idx = self._name_map[name]
             self.components[idx] = value
         else:
@@ -105,37 +125,69 @@ class Vector:
         return iter(self.components)
     
     def __hash__(self):
-        # allows the vector to be used in sets or as dict keys
-        return hash(tuple(self.components))
+        # each object in python has a unique hash like their fingerprint and python uses them in things like sets and dictionary as keys
+        # in short, this allows the vector to be used in sets or as dict keys
+        if self.mutable:
+            # if an object is mutable it is very dangerous to depend on its hash because it can change later and this is defeats the purpose of depending on hashes in the first place
+            raise TypeError("Mutable vectors cannot be hashed.")
+        
+        return hash(self.components)
 
     # Binary Operators
     def __add__(self, other):
-        if not self._validate_vector(other, "addition"):
-            return NotImplemented
-        return Vector([a + b for a, b in zip(self.components, other.components)])
+        if self._validate_vector(other):
+            return Vector([a + b for a, b in zip(self.components, other.components)], mutable=self.mutable)
+        
+        # < 1, 2, 3 > + 5 = < 6, 7, 8 >
+        if isinstance(other, (int, float)):
+            return Vector([x + other for x in self.components], mutable=self.mutable)
+        
+        return NotImplemented
     
     def __radd__(self, other):
         return self + other
 
     def __iadd__(self, other):
-        if not self._validate_vector(other, "addition"):
-            return NotImplemented
-        self.components = [a + b for a, b in zip(self.components, other.components)]
-        return self
+        if self.mutable:
+            if self._validate_vector(other):
+                self.components = [a + b for a, b in zip(self.components, other.components)]
+                return self
+            elif isinstance(other, (int, float)):
+                self.components = [x + other for x in self.components]
+                return self
+        else:
+            # if the vector is immutable then return a new vector
+            return self + other
+        
+        return NotImplemented
 
     def __sub__(self, other):
-        if not self._validate_vector(other, "subtraction"):
-            return NotImplemented
-        return Vector([a - b for a, b in zip(self.components, other.components)])
+        if self._validate_vector(other):
+            return Vector([a - b for a, b in zip(self.components, other.components)], mutable=self.mutable)
+        
+        # < 1, 2, 3 > - 5 = < -4, -3, -2 >
+        if isinstance(other, (int, float)):
+            return Vector([x - other for x in self.components], mutable=self.mutable)
+        
+        return NotImplemented
     
     def __rsub__(self, other):
-        return self - other
+        # -(self - other) = -self + other = other - self. we did this to be able to use the implementation in __sub__
+        return -(self - other)
     
     def __isub__(self, other):
-        if not self._validate_vector(other, "addition"):
-            return NotImplemented
-        self.components = [a - b for a, b in zip(self.components, other.components)]
-        return self
+        if self.mutable:
+            if self._validate_vector(other):
+                self.components = [a - b for a, b in zip(self.components, other.components)]
+                return self
+            elif isinstance(other, (int, float)):
+                self.components = [x - other for x in self.components]
+                return self
+        else:
+            # if the vector is immutable then return a new vector
+            return self - other
+        
+        return NotImplemented
 
     def __mul__(self, other):
         # handles multiplying by a scalar
@@ -143,14 +195,16 @@ class Vector:
             return Vector([x * other for x in self.components])
         
         # handles multiply by another vector and performs dot product not cross product
-        if self._validate_vector(other, "addition"):
+        if self._validate_vector(other):
             return sum(a * b for a, b in zip(self.components, other.components))
+
+        # vector * matrix is defined in the matrix class
 
         return NotImplemented
     
     # handles case where the scalar is on the right of the vector. 5 * vector
     def __rmul__(self, other):
-        return self.__mul__(other)
+        return self * other
     
     def __imul__(self, other):
         # handles multiplying by a scalar
@@ -168,10 +222,10 @@ class Vector:
 
     # Unary Operators
     def __pos__(self):
-        return Vector(self.components[:])
+        return Vector(self.components[:], mutable=self.mutable)
 
     def __neg__(self):
-        return Vector([-x for x in self.components])
+        return Vector([-x for x in self.components], mutable=self.mutable)
 
     def __abs__(self):
         return self.magnitude()
@@ -180,9 +234,12 @@ class Vector:
     def __eq__(self, other):
         if not isinstance(other, Vector):
             return False
-        return self.components == other.components
+        return self.is_close(other)
     
-    def is_close(self, other, tol=1e-9):
+    def __ne__(self, other):
+        return not self == other
+    
+    def is_close(self, other, tol=EPSILON):
         if not isinstance(other, Vector) or len(self) != len(other):
             return False
         
@@ -191,7 +248,13 @@ class Vector:
 
     # returns False if all components of a vector are zero
     def __bool__(self):
-        return any(self.components)
+        return any(abs(x) > self.EPSILON for x in self.components)
+    
+    def is_zero_vector(self):
+        return not bool(self)
+
+    def is_unit_vector(self):
+        return math.isclose(self.magnitude(), 1.0, rel_tol=self.EPSILON)
 
     # Vector Functions
     def magnitude(self):
@@ -205,43 +268,107 @@ class Vector:
         new_y = self.z * other.x - self.x * other.z
         new_z = self.x * other.y - self.y * other.x
         
-        return Vector([new_x, new_y, new_z])
+        return Vector([new_x, new_y, new_z], mutable=self.mutable)
     
-    def unit_vector(self):
+    def normalize(self):
         magnitude_value = self.magnitude()
-        return Vector([x / magnitude_value for x in self.components])
+        return Vector([x / magnitude_value for x in self.components], mutable=self.mutable)
 
-    def project_onto_scalar_factor(self, other):
-        if self._validate_vector(other, "projection onto another vector"):
+    def scalar_projection_onto(self, other):
+        if self._validate_vector(other):
             magnitude_other_squared = other * other # i.e. other dotted with itself
 
-            if magnitude_other_squared == 0:
+            if magnitude_other_squared <= self.EPSILON:
                 raise ValueError("Cannot project onto a zero vector.")
             
             dot_product = self * other
 
-            scalar_factor = dot_product / magnitude_other_squared
+            return dot_product / magnitude_other_squared
 
-            return scalar_factor
-
-    def project_onto_vector(self, other):            
-            scalar_factor = self.project_onto_scalar_factor(other)
-
+    def project_onto(self, other):            
+            scalar_factor = self.scalar_projection_onto(other)
             return scalar_factor * other
+    
+    @staticmethod
+    def are_parallel(v1, v2, allow_antiparallel=True, tol=EPSILON):
+        
+        if not Vector.same_dimensions(v1, v2):
+            raise ValueError(f"Dimension mismatch: {len(v1)} and {len(v2)}")
+        
+        if v1.is_zero_vector() or v2.is_zero_vector():
+            return False
+        
+        u1 = v1.normalize()
+        u2 = v2.normalize()
+
+        dot_product = u1 * u2
+
+        if allow_antiparallel:
+            return math.isclose(abs(dot_product), 1.0, abs_tol=tol)
+        else:
+            return math.isclose(dot_product, 1.0, abs_tol=tol)
+
+    @staticmethod
+    def are_antiparallel(v1, v2, tol=EPSILON):
+        if not Vector.same_dimensions(v1, v2):
+            raise ValueError(f"Dimension mismatch: {len(v1)} and {len(v2)}")
+        
+        if v1.is_zero_vector() or v2.is_zero_vector():
+            return False
+        
+        u1 = v1.normalize()
+        u2 = v2.normalize()
+
+        dot_product = u1 * u2
+
+        return math.isclose(dot_product, -1.0, abs_tol=tol)
+
+    @staticmethod
+    def are_all_parallel(*vectors, allow_antiparallel=True, tol=EPSILON):
+        if len(vectors) < 2:
+            return True
+        
+        # idea is if a // b and a // c and a // d then for sure b // c // d
+        anchor = vectors[0]
+
+        for v in vectors[1:]:
+            if not Vector.are_parallel(anchor, v, allow_antiparallel=allow_antiparallel, tol=tol):
+                return False
+            
+        return True
+    
+    @staticmethod
+    def angle_between(v1, v2, in_degrees=False):
+        if not Vector.same_dimensions(v1, v2):
+            raise ValueError(f"Dimension mismatch: {len(v1)} and {len(v2)}")
+        
+        if v1.is_zero_vector() or v2.is_zero_vector():
+            raise ValueError("Angle is undefined with a zero vector.")
+        
+        u1 = v1.normalize()
+        u2 = v2.normalize()
+
+        dot_product = max(-1.0, min(1.0, u1 * u2))
+
+        angle = math.acos(dot_product)
+
+        return math.degrees(angle) if in_degrees else angle
+    
+    @staticmethod
+    def are_perpendicular(v1, v2, tol=EPSILON):
+        if not Vector.same_dimensions(v1, v2):
+            raise ValueError(f"Dimension mismatch: {len(v1)} and {len(v2)}")
+        
+        dot_product = v1 * v2
+
+        return math.isclose(dot_product, 0.0, abs_tol=tol)
+        
+    # TODO mean vector between vectors - angular spread - linear dependence
 
     # method aliases
     mag = magnitude
-
-# # --- Testing the flexibility ---
-# if __name__ == "__main__":
-#     # Works with a list (Unlimited items)
-#     v1 = Vector([1, 2, 3, 4, 5, 6])
-#     print(f"List init: {v1.x}, {v1[5]}") # 1, 6
-
-#     # Works with keywords
-#     v2 = Vector(x=10, z=30)
-#     print(f"Keyword init: {v2.components}") # [10, 0, 30]
-
-#     # Works with positional arguments
-#     v3 = Vector(1, 2)
-#     print(f"Positional: {v3.x}, {v3.y}") # 1, 2
+    get_unit_vector = normalize
+    norm = normalize
+    are_orthogonal = are_perpendicular
+    is_zero = is_zero_vector
+    is_unit = is_unit_vector
